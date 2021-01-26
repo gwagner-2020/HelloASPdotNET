107e6ea51b741ea35a6d9312f40397982b1ba351
Gretchen Wagner <gretchenwag@gmail.com>
2021-01-26 13:58:53 -0600

    completed chap13-5

--- HelloASPdotNET/Controllers/HelloController.cs
+++ HelloASPdotNET/Controllers/HelloController.cs
@@ -4,81 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace HelloASPdotNET.Controllers
+namespace HelloASPDotNET.Controllers
 {
-    //[Route("/helloworld/")]
     public class HelloController : Controller
     {
-    // GET route now /helloworld (no longer default route /Hello/Index)
-    // GET /helloworld/
-    [HttpGet] //this attribute explicitly tells to be GET request - GET is default
-              //[Route("/helloworld/")] //this attribute overrides default route /Hello/Index -- case insensitive
-    [Route("/helloworld/")]
-    public IActionResult Index()
-    {
-        return View("HelloForm");
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View("HelloForm");
+        }
+
+        [HttpPost]
+        [Route("/hello")]
+        public IActionResult Welcome(string name = "World")
+        {
+            ViewBag.person = name;
+            return View();
+            //return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        }
     }
-
-    // GET: /Hello/Welcome
-    // "World" is a default value if nothing is passed in to query
-    // possible to use two parameters
-    //public IActionResult Welcome(string name = "World", string farewell = "Bye")
-    //[HttpGet]
-    //[HttpGet("welcome/{firstName?}/")]
-    //[HttpPost("welcome/")]
-    //[Route("/helloworld/Welcome/{firstName?}")]
-    [HttpPost]
-    [Route("/helloworld/")]
-    public IActionResult Welcome(string firstName = "World", string language = "english")
-    {
-        //CreateMessage(firstName, language);
-
-        //string html = "<h1>" + "Welcome to my app, " + firstName + "</h1>";
-        //html += "<h2>" + farewell + "</h2>";
-
-        string returnGreeting = CreateMessage(firstName, language);
-
-        string html = "<h1 style='color: blue;'>" + returnGreeting + ", " + firstName + "</h1>";
-
-        return Content(html, "text/html");
-    }
-
-    public static string CreateMessage(string firstName, string language)
-    {
-        string greeting = "";
-
-        if (language == "english")
-        {
-            greeting = "Hello";
-            //return "Hello, " + firstName;
-        }
-
-        else if (language == "french")
-        {
-            greeting = "Bonjour";
-            //return "Bonjour, " + firstName;
-        }
-        else if (language == "esperanto")
-        {
-            greeting = "Saluton";
-            // return "Saluton, " + firstName;
-        }
-        else if (language == "German")
-        {
-            greeting = "Guten Tag";
-            // return "Guten Tag, " + firstName;
-        }
-        else
-        {
-            greeting = "Hello";
-            //return "Hello, " + firstName;
-        }
-
-        return greeting;
-
-
-    }
-  }
-
 }
-
